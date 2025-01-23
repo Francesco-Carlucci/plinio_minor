273107e7f16cc74510b0de43161edd0ca5b1d109
Francesco-Carlucci <carlucci.francesco2000@gmail.com>
2025-01-23 16:12:56 +0100

    modifying versions to distinguish from plinio official

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='plinio',
-    version='0.0.1',
+    version='0.0.2',
     packages=setuptools.find_packages(),
     install_requires=['setuptools'],
     maintainer='Daniele Jahier Pagliari',
