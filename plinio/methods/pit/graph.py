2dd41d3f6997f0b0054649a12faa971188f85a24
Francesco-Carlucci <carlucci.francesco2000@gmail.com>
2025-03-06 11:33:12 +0100

    removing exclude names and types from build_shared_features_map

--- plinio/methods/pit/graph.py
+++ plinio/methods/pit/graph.py
@@ -107,7 +107,7 @@ def convert(model: nn.Module, input_example: Any, conversion_type: str,
     add_node_properties(mod)
     if conversion_type in ('autoimport', 'export'):
         # dictionary of shared feature maskers. Used only in 'autoimport' mode.
-        sm_dict = {} if conversion_type != 'autoimport' else build_shared_features_map(mod, exclude_names, exclude_types)
+        sm_dict = {} if conversion_type != 'autoimport' else build_shared_features_map(mod) #, exclude_names, exclude_types)
         if conversion_type == 'export':
             add_features_calculator(mod, [pit_features_calc])
         convert_layers(mod, conversion_type, sm_dict, exclude_names, exclude_types, fold_bn)
@@ -164,9 +164,7 @@ def convert_layers(mod: fx.GraphModule,
     return
 
 
-def build_shared_features_map(mod: fx.GraphModule,
-            exclude_names: Iterable[str] = (),
-            exclude_types: Iterable[Type[nn.Module]] = (),) -> Dict[fx.Node, PITFeaturesMasker]:
+def build_shared_features_map(mod: fx.GraphModule,) -> Dict[fx.Node, PITFeaturesMasker]:
     """Create a map from fx.Node instances to instances of PITFeaturesMasker to be used by PIT
     to optimize the number of features of that node. Handles the sharing of masks among
     multiple nodes.
@@ -240,9 +238,9 @@ def build_shared_features_map(mod: fx.GraphModule,
             if n.meta['features_concatenate']:
                 #predecessors = n.meta['predecessors']
                 input_sm = [sm_dict[ni] for ni in n.meta['predecessors']]
-                for i,p in enumerate(n.meta['predecessors']):
-                    if is_layer(p, mod, tuple(pit_layer_map.values())):
-                        input_sm[i] = mod.get_submodule(str(p.target)).out_features_masker
+                #for i,p in enumerate(n.meta['predecessors']):
+                #    if is_layer(p, mod, tuple(pit_layer_map.values())):
+                #        input_sm[i] = mod.get_submodule(str(p.target)).out_features_masker
                 new_sm = PITConcatFeaturesMasker(input_sm)
                 for n in c:
                     sm_dict[n] = new_sm
